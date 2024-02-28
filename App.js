17f2af13ba5855ac6484da4642ea9fbce093eb94
Moha0170 <mohalaja06@gmail.com>
2024-02-28 09:19:05 +0100

    adatok beirodnak a json fileba

--- App.js
+++ App.js
@@ -2,8 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const path = require('path');
+const fs = require('fs');
 
+const JSON_FILE_PATH = path.join(__dirname, '/public/adatok.json');
 app.use(express.static(__dirname + '/public'));
+app.use(express.json());
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '/public/index.html'));
@@ -15,4 +18,38 @@ app.listen(port, () => {
 
 app.get('/json/:valami', function(req, res) {
     console.log(req.params.valami)
+})
+
+app.get('/bevitel/:etelNeve/:kaloria/:tomeg', function(req, res) {
+  console.log(req.params.etelNeve, req.params.kaloria, req.params.tomeg);
+  fs.readFile(JSON_FILE_PATH, (err, data) => {
+    if (err) {
+        console.error('Error reading file:', err);
+        res.status(500).send('Error reading file');
+        return;
+    }
+
+    let existingData = [];
+    if (data.length > 0) {
+        existingData = JSON.parse(data);
+    }
+
+    var newData = {
+      "etelNeve": req.params.etelNeve,
+      "kaloria": req.params.kaloria,
+      "tomeg": req.params.tomeg
+    };
+
+    existingData.push(newData);
+
+    fs.writeFile(JSON_FILE_PATH, JSON.stringify(existingData, null, 2), (err) => {
+        if (err) {
+            console.error('Error writing file:', err);
+            res.status(500).send('Error writing file');
+            return;
+        }
+        console.log('Data written successfully');
+        res.status(200).send('Data written successfully');
+    });
+  });
 })
--- public/client.js
+++ public/client.js
@@ -1,6 +1,11 @@
-var xhr = XMLHttpRequest();
-var valami = 10
-var url = 'http://localhost:3000/json/' + valami;
+var xhr = new XMLHttpRequest();
 
-xhr.open('GET', url);
-xhr.send()
+document.getElementById("bevitel").addEventListener("click", () =>{
+    var etel = document.getElementById("nev").value 
+    var kaloria = document.getElementById("kaloriaszaz").value 
+    var tomeg = document.getElementById("tomeg").value
+    var url = "http://localhost:3000/bevitel/" +  etel + "/" + kaloria + "/" + tomeg;
+    xhr.open("GET", url);
+    xhr.send();
+
+})
