cd72f0ffc345ffa54f0be9e8f774a646cd4d040d
bnolod <sadfoolc@gmail.com>
2024-03-02 11:08:00 +0100

    date megoldas nincskesz dolgozni kell

--- public/client.js
+++ public/client.js
@@ -1,13 +1,30 @@
 var xhr = new XMLHttpRequest();
 var datum = new Date()
 
-document.getElementById("bevitel").addEventListener("click", () =>{
-    var date = datum.now();
+function formatDate(date) {
+    var d = new Date(date),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2) 
+        month = '0' + month;
+    if (day.length < 2) 
+        day = '0' + day;
+
+    return [year, month, day].join('-');
+}
+
+
+document.getElementById("bevitel").addEventListener("click", (event) =>{
+    event.preventDefault();
+    var date = Date.now();
+    // console.log(formatDate(date))
     var etel = document.getElementById("nev").value 
     var kaloria = document.getElementById("kaloriaszaz").value 
     var tomeg = document.getElementById("tomeg").value
-    var url = "http://localhost:3000/bevitel/" +  etel + "/" + kaloria + "/" + tomeg + "/" + date;
+    var url = "http://localhost:3000/bevitel/" +  etel + "/" + kaloria + "/" + tomeg + "/" + formatDate(date);
     xhr.open("GET", url);
     xhr.send();
-
+    
 })
