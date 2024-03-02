00f54b4d02066ba08c9c8920c99893b70088c7cc
bnolod <sadfoolc@gmail.com>
2024-03-02 12:38:51 +0100

    kaloria js

--- public/viewData.js
+++ public/viewData.js
@@ -1,5 +1,30 @@
 
-function updateScreen() {
+async function BevittKaloria(date) {
 
-    document.getElementById("bevitt").innerHTML = "Bevitt kalória: " + bevittKaloria + " kcal";
+    const response = await fetch(`/data/${date}`);
+    const data = await response.json();
+
+    kcal = 0;
+    data.forEach(element => {
+        
+    });
+
+
+    return kcal;
+}
+async function maxKaloria() {
+    const response = await fetch(`/kcal`);
+    const data = await response.json();
+
+    kcal = data.cel;
+    return kcal;
+    
+}
+
+function updateScreen(date) {
+    maxKaloria= maxKaloria();
+    bevittKcal = BevittKaloria(date);
+    
+    document.getElementById("bevitt").innerHTML = `Bevitt kalória: ${bevittKcal}/${maxKaloria} kcal`;
+
 }
