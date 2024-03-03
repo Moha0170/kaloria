3f29883957344091996dbfc9340b64eb8a9aa113
bnolod <sadfoolc@gmail.com>
2024-03-03 14:10:52 +0100

    kilistazas

--- public/client.js
+++ public/client.js
@@ -1,62 +1,65 @@
-var xhr = new XMLHttpRequest();
-var datum = new Date()
-
-function formatDate(date) {
-    var d = new Date(date),
-        month = '' + (d.getMonth() + 1),
-        day = '' + d.getDate(),
-        year = d.getFullYear();
-
-    if (month.length < 2) 
-        month = '0' + month;
-    if (day.length < 2) 
-        day = '0' + day;
-
-    return [year, month, day].join('-');
-}
+// var xhr = new XMLHttpRequest();
+// var datum = new Date()
 
 
-document.getElementById("bevitel").addEventListener("click", (event) =>{
-    var kaka = document.getElementById("kaka")
-    var date = Date.now();
-    // console.log(formatDate(date))
-    var etel = document.getElementById("nev").value 
-    var kaloria = document.getElementById("kaloriaszaz").value 
-    var tomeg = document.getElementById("tomeg").value
+// function formatDate(date) {
+//     var d = new Date(date),
+//         month = '' + (d.getMonth() + 1),
+//         day = '' + d.getDate(),
+//         year = d.getFullYear();
 
-    if (kaka.style.visibility === "visible") {
-        var url = "http://localhost:3000/bevitel/" +  etel + "/" + kaloria + "/" + tomeg + "/" + formatDate(date);
-    }
-    else {
-        var url = "http://localhost:3000/bevitel/" +  etel + "/" + kaloria + "/" + 0 + "/" + formatDate(date);
-    }
+//     if (month.length < 2) 
+//         month = '0' + month;
+//     if (day.length < 2) 
+//         day = '0' + day;
+
+//     return [year, month, day].join('-');
+// }
+
+
+// document.getElementById("bevitel").addEventListener("click", (event) =>{
+//     event.preventDefault();
+//     var kaka = document.getElementById("kaka")
+//     var date = document.getElementById("dateText").innerHTML;
+//     // console.log(formatDate(date))
+//     var etel = document.getElementById("nev").value 
+//     var kaloria = document.getElementById("kaloriaszaz").value 
+//     var tomeg = document.getElementById("tomeg").value
+
+//     if (kaka.style.visibility === "visible") {
+//         var url = "http://localhost:3000/bevitel/" +  etel + "/" + kaloria + "/" + tomeg + "/" + date;
+//     }
+//     else {
+//         var url = "http://localhost:3000/bevitel/" +  etel + "/" + kaloria + "/" + 0 + "/" + date;
+//     }
 
     
-    xhr.open("GET", url);
-    xhr.send();
-})
-document.getElementById("celgomb").addEventListener("click", (event) =>{
-    var cel = document.getElementById("cel").value;
-    var url = "http://localhost:3000/celgomb/" +  cel;
-    xhr.open("GET", url);
-    xhr.send();
-})
+//     xhr.open("GET", url);
+//     xhr.send();
+//     updateScreen(date);
+// })
+// document.getElementById("celgomb").addEventListener("click", (event) =>{
+//     var cel = document.getElementById("cel").value;
+//     var url = "http://localhost:3000/celgomb/" +  cel;
+//     xhr.open("GET", url);
+//     xhr.send();
+// })
 
 
 
-document.getElementById("valtb").addEventListener("click", (event) =>{
+// document.getElementById("valtb").addEventListener("click", (event) =>{
 
-    var kaka = document.getElementById("kaka")
-    var kalertek = document.getElementById("kalertek")
+//     var kaka = document.getElementById("kaka")
+//     var kalertek = document.getElementById("kalertek")
 
-    if (kaka.style.visibility === "visible") {
-        kaka.style.visibility = "hidden";
-        kaka.style.maxHeight = "0px";
-        kalertek.innerHTML = "Kalória érték:";
+//     if (kaka.style.visibility === "visible") {
+//         kaka.style.visibility = "hidden";
+//         kaka.style.maxHeight = "0px";
+//         kalertek.innerHTML = "Kalória érték:";
 
-    } else {
-        kaka.style.visibility = "visible";
-        kaka.style.maxHeight = "1000px";
-        kalertek.innerHTML = "Kalória érték /100g:";
-    }
-})
+//     } else {
+//         kaka.style.visibility = "visible";
+//         kaka.style.maxHeight = "1000px";
+//         kalertek.innerHTML = "Kalória érték /100g:";
+//     }
+// })
--- public/viewData.js
+++ public/viewData.js
@@ -1,3 +1,6 @@
+var xhr = new XMLHttpRequest();
+var datum = new Date()
+
 var defDate = formatDate(Date.now());
 
 async function BevittKaloria(date) {
@@ -56,12 +59,99 @@ function dateChange(change, date){
     return formatDate(presentDate);
 }
 
-async function updateScreen(date = "2024-03-02") {
-    maxKaloria= await maxKaloria();
+async function updateScreen(date = defDate) {
+    maxKcal= await maxKaloria();
     bevittKcal = await BevittKaloria(date);
+    document.getElementById("dateText").innerHTML = date;
     
-    document.getElementById("bevitt").innerHTML = `Bevitt kalória: ${bevittKcal}/${maxKaloria} kcal`;
+    document.getElementById("bevitt").innerHTML = `Bevitt kalória: ${bevittKcal}/${maxKcal} kcal`;
+    document.getElementById("felvettLista").innerHTML = await Listazas(date);
 
 }
 
-updateScreen("2024-03-02");
+document.getElementById("date0").addEventListener("click", (event) =>{
+    defDate = dateChange(0, defDate);
+    updateScreen(defDate);
+})
+document.getElementById("date1").addEventListener("click", (event) =>{
+    defDate = dateChange(1, defDate);
+    updateScreen(defDate);
+})
+
+updateScreen(defDate);
+
+
+async function Listazas(date) {
+
+    const response = await fetch(`/data/${date}`);
+    const data = await response.json();
+
+    output = "";
+
+
+    data.forEach(element => {
+        kcal = 0;
+        if (element.tomeg == 0) {
+            kcal += parseInt(element.kaloria);
+        } else {
+            kcal+= element.kaloria*(element.tomeg/100);
+        }
+
+        output+= `<div>
+        <h1>${element.etelNeve}</h1>
+        <p>${kcal} kcal</p>
+        </div>`
+    });
+
+
+    return output;
+}
+
+document.getElementById("bevitel").addEventListener("click", (event) =>{
+    event.preventDefault();
+    var kaka = document.getElementById("kaka")
+    var date = document.getElementById("dateText").innerHTML;
+    // console.log(formatDate(date))
+    var etel = document.getElementById("nev").value 
+    var kaloria = document.getElementById("kaloriaszaz").value 
+    var tomeg = document.getElementById("tomeg").value
+
+    if (kaka.style.visibility === "visible") {
+        var url = "http://localhost:3000/bevitel/" +  etel + "/" + kaloria + "/" + tomeg + "/" + date;
+    }
+    else {
+        var url = "http://localhost:3000/bevitel/" +  etel + "/" + kaloria + "/" + 0 + "/" + date;
+    }
+
+    
+    xhr.open("GET", url);
+    xhr.send();
+    updateScreen(date);
+})
+document.getElementById("celgomb").addEventListener("click", (event) =>{
+    var cel = document.getElementById("cel").value;
+    var url = "http://localhost:3000/celgomb/" +  cel;
+    xhr.open("GET", url);
+    xhr.send();
+})
+
+
+
+document.getElementById("valtb").addEventListener("click", (event) =>{
+
+    var kaka = document.getElementById("kaka")
+    var kalertek = document.getElementById("kalertek")
+
+    if (kaka.style.visibility === "visible") {
+        kaka.style.visibility = "hidden";
+        kaka.style.maxHeight = "0px";
+        kalertek.innerHTML = "Kalória érték:";
+
+    } else {
+        kaka.style.visibility = "visible";
+        kaka.style.maxHeight = "1000px";
+        kalertek.innerHTML = "Kalória érték /100g:";
+    }
+})
+
+// lajos nem csinalt semmit adjanak neki egyest koszi
